afa2b52939400ad608004813c0a4762fa2e213fa
Maxsuel <maxsuel.lima@dcx.ufpb.br>
2026-07-30 19:29:30 -0300

    feat - implementando exercicio 217. Contains Duplicate

--- src/main/java/Maxbr221/github/icompas/leet04/ContainsDuplicate.java
+++ src/main/java/Maxbr221/github/icompas/leet04/ContainsDuplicate.java
@@ -1,6 +1,21 @@
 package Maxbr221.github.icompas.leet04;
 
+import java.util.HashSet;
+
 public class ContainsDuplicate {
+
+
+    public boolean ContainsDuplicate(int[] nums){
+        HashSet<Integer> vistos = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if(vistos.contains(nums[i])){
+                return true;
+            }else{
+                vistos.add(nums[i]);
+            }
+        }
+        return false;
+    }
 
 
 
