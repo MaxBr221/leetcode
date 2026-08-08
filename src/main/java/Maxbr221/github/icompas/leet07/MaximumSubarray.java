4c128243ac469051bc70216cbc4d09170a826c2b
Maxsuel <maxsuel.lima@dcx.ufpb.br>
2026-08-08 19:21:28 -0300

    feat - finalizando exercicio MaximumSubarray

--- src/main/java/Maxbr221/github/icompas/leet07/MaximumSubarray.java
+++ src/main/java/Maxbr221/github/icompas/leet07/MaximumSubarray.java
@@ -2,7 +2,21 @@ package Maxbr221.github.icompas.leet07;
 
 public class MaximumSubarray {
 
+    //nums = [-2,1,-3,4,-1,2,1,-5,4]
     public int maxSubArray(int[] nums) {
+        int somaAtual = nums[0];
+        int maiorSoma = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if(somaAtual + nums[i] > nums[i]){
+                somaAtual = somaAtual + nums[i];
 
+            }else{
+                somaAtual = nums[i];
+            }
+            if(somaAtual > maiorSoma){
+                maiorSoma = somaAtual;
+            }
+        }
+        return maiorSoma;
     }
 }
