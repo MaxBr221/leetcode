c1fc596eef30d08cde2b6a3d337c6346fff910b1
Maxsuel <maxsuel.lima@dcx.ufpb.br>
2026-08-17 23:01:24 -0300

    feat - finalizando exercicio FindPivotIndex

---
+++ src/main/java/Maxbr221/github/icompas/leet10/FindPivotIndex.java
@@ -0,0 +1,25 @@
+package Maxbr221.github.icompas.leet10;
+
+public class FindPivotIndex {
+
+    public int pivotIndex(int[] nums) {
+        int[] sumLeft = new int[nums.length];
+        int[] sumRight = new int[nums.length];
+        //[1,7,3,6,5,6]
+        sumLeft[0] = 0;
+        for (int i = 1; i < nums.length; i++) {
+            sumLeft[i] = sumLeft[i - 1] + nums[i - 1];
+        }
+        sumRight[nums.length - 1] = 0;
+        for (int i = nums.length -2; i >= 0; i--) {
+            sumRight[i] = sumRight[i + 1] + nums[i + 1];
+
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if(sumLeft[i] == sumRight[i]){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
