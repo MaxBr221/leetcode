a677c3b30bcabf8abc01988c81110032c32cf565
Maxsuel <maxsuel.lima@dcx.ufpb.br>
2026-08-23 13:42:59 -0300

    feat - finalizando exercicio ReverseString

---
+++ src/main/java/Maxbr221/github/icompas/leet11/ReverseString.java
@@ -0,0 +1,15 @@
+package Maxbr221.github.icompas.leet11;
+
+public class ReverseString {
+
+    public void reverseString(char[] s) {
+        int i = 0;
+        int j = s.length -1;
+        //["h","e","l","l","o"]
+        for (i = 0; i < j; i++, j--) {
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+        }
+    }
+}
