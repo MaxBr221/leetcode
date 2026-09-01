4449f7dbc8b49d04942ab0f26170255d4e1551ef
Maxsuel <maxsuel.lima@dcx.ufpb.br>
2026-09-01 19:04:44 -0300

    feat - implementando exercicio RichestCustomerWealth

---
+++ src/main/java/Maxbr221/github/leet15/RichestCustomerWealth.java
@@ -0,0 +1,18 @@
+package Maxbr221.github.leet15;
+
+public class RichestCustomerWealth {
+
+    public int maximumWealth(int[][] accounts) {
+        int maiorValor = 0;
+        for (int i = 0; i < accounts.length; i++) {
+            int soma = 0;
+            for (int j = 0; j < accounts[i].length; j++) {
+                soma += accounts[i][j];
+            }
+            if(soma > maiorValor){
+                maiorValor = soma;
+            }
+        }
+        return maiorValor;
+    }
+}
